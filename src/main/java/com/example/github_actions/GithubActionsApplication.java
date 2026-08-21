5ccbbe2468f3407561ad938df59054201f2c1d4b
achary-hinal <hinal@starcsec.com>
2026-08-21 16:47:30 +0400

    Update GithubActionsApplication.java

--- src/main/java/com/example/github_actions/GithubActionsApplication.java
+++ src/main/java/com/example/github_actions/GithubActionsApplication.java
@@ -7,19 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionsApplication {
 
 	//private static final String DB_PASSWORD = "Admin@1245";
-    private static final String API_SECRET_KEY = "sk_live_8f7a9d123456789";
-    private static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiJ9.test.token";
-    private static final String AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
+    // private static final String API_SECRET_KEY = "sk_live_8f7a9d123456789";
+    // private static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiJ9.test.token";
+    // private static final String AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
 
 	public static void main(String[] args) {
 	
 		
-		//SpringApplication.run(GithubActionsApplication.class, args); 
+		SpringApplication.run(GithubActionsApplication.class, args); 
 
 		//System.out.println("Database Password: " + DB_PASSWORD);
-        System.out.println("API Secret: " + API_SECRET_KEY);
-        System.out.println("JWT Token: " + JWT_TOKEN);
-        System.out.println("AWS Key: " + AWS_ACCESS_KEY);
+        // System.out.println("API Secret: " + API_SECRET_KEY);
+        // System.out.println("JWT Token: " + JWT_TOKEN);
+        // System.out.println("AWS Key: " + AWS_ACCESS_KEY);
 		
 		
 	}
