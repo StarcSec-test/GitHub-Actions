9f2c597db097af2909d56991bee4a64a639930d0
achary-hinal <hinal@starcsec.com>
2026-08-21 16:52:11 +0400

    Update GithubActionsApplication.java

--- src/main/java/com/example/github_actions/GithubActionsApplication.java
+++ src/main/java/com/example/github_actions/GithubActionsApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsApplication.class, args); 
+		//SpringApplication.run(GithubActionsApplication.class, args); 
+		private static final String AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
+		System.out.println("AWS Key: " + AWS_ACCESS_KEY);
 	}
 }
