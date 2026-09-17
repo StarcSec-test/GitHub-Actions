74b63bfad3a2c75e51159b8d30d384f34bc5038f
achary-hinal <hinal@starcsec.com>
2026-09-17 15:04:26 +0400

    Create env.prod.ts

---
+++ src/main/java/com/example/github_actions/env.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '********************************',
+    authDomain: '********************************',
+    projectId: '********************************',
+    storageBucket: '********************************',
+    messagingSenderId: '********************************',
+    appId: '********************************',
+    measurementId: '********************************',
+  },
+  // API_URL: 'http://localhost:8989/api',
+  // AGENT_CONFIG_URL: 'https://staging.starcsec.com/api',
+  // API_URL: 'https://staging.starcsec.com:8443/dev_api',
+  // API_URL: 'https://dev.starcsec.com/dev_api',
+  API_URL: 'https://api.starcsec.com',
+};
